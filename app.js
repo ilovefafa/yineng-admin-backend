375fdb9038258c8b2809d412da4f72afe793aaf8
ilovefafa <3373198511@qq.com>
2018-07-22 17:31:02 +0800

    deploy

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//test
+// var fs = require('fs')
+const path = require('path');
+
 //数据库配置
 require("./mongoose/index.js")
 //koa实例
@@ -6,12 +10,20 @@ const app = new Koa();
 //跨域
 let cors = require('koa2-cors');
 app.use(cors({
-    origin: 'http://localhost:9528',
+  origin: '*',
 }));
 //解析数据
 const koaBody = require('koa-body');
 app.use(koaBody());
+//静态资源
+const static = require('koa-static')
+// 配置静态资源
+const staticPath = './dist'
+app.use(static(
+  path.join(__dirname, staticPath)
+))
 //路由
 require("./route")(app)
 
-app.listen(3000);
+
+app.listen(3001);
